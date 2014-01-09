2c356a1bb57d6263fc24b8afceeb941991436daf
Adam Blair <adamblair@rogers.com>
2014-01-09 00:34:20 -0500

    Added blocks and items
    BLOCKS
    +Added Limestone
    +Added LandMine
    
    ITEMS
    +Added Lime

--- ExampleMod/common/ExampleMod.java
+++ ExampleMod/common/ExampleMod.java
@@ -1,6 +1,7 @@
 package evar678.ExampleMod.common;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
@@ -14,10 +15,12 @@ import cpw.mods.fml.common.registry.LanguageRegistry;
 import cpw.mods.fml.common.network.NetworkMod.SidedPacketHandler;
 import cpw.mods.fml.common.SidedProxy;
 import evar678.ExampleMod.common.blocks.BlockLimestone;
+import evar678.ExampleMod.common.blocks.BlockLandMine;
 import evar678.ExampleMod.common.handlers.ExampleModClientPacketHandler;
 import evar678.ExampleMod.common.handlers.ExampleModServerPacketHandler;
 import evar678.ExampleMod.common.handlers.ExampleModServerPacketHandler;
 import evar678.ExampleMod.common.handlers.ExampleModClientPacketHandler;
+import evar678.ExampleMod.common.items.ItemLime;
 
 @NetworkMod(clientSideRequired = true, serverSideRequired = true, clientPacketHandlerSpec = @SidedPacketHandler(channels = { "ExampleMod" }, packetHandler = ExampleModClientPacketHandler.class), serverPacketHandlerSpec = @SidedPacketHandler(channels = { "ExampleMod" }, packetHandler = ExampleModServerPacketHandler.class))
 
@@ -33,19 +36,23 @@ public class ExampleMod
 	// Tells Forge the location of your proxies
 	public static ExampleModCommonProxy proxy;
 	
+	//ITEMS
+	public static Item Lime;
+	
 	// BLOCKS
 	public static Block Limestone;
+	public static Block LandMine;
 	
 	@PreInit
 	public void PreInit(FMLPreInitializationEvent e)
 	{
 		
 		// BLOCKS
-		Limestone = new BlockLimestone(3000).setUnlocalizedName("Limestone"); // 3000
-																				// is
-																				// its
-																				// ID
+		Limestone = new BlockLimestone(3000).setUnlocalizedName("Limestone"); // 3000 is its ID
+		LandMine = new BlockLandMine(3001).setUnlocalizedName("LandMine");
 		
+		//ITEMS
+		Lime = new ItemLime(5000).setUnlocalizedName("Lime");
 	}
 	
 	@Init
@@ -57,7 +64,9 @@ public class ExampleMod
 		// BLOCKS (METHOD)
 		proxy.registerBlocks(); // Calls the registerBlocks method -- This
 								// wasn't here before, so don't skip over this!
-		
+		//ITEMS (METHOD)
+		proxy.registerItems(); //DON'T FORGET THIS, ADD IT IN AND THEN DELETE THIS REMINDER COMMENT
+
 		// MULTIPLAYER ABILITY
 		NetworkRegistry.instance().registerGuiHandler(this, proxy); // Registers
 																	// the class
--- ExampleMod/common/ExampleModCommonProxy.java
+++ ExampleMod/common/ExampleModCommonProxy.java
@@ -41,16 +41,22 @@ public class ExampleModCommonProxy implements IGuiHandler
 	public void registerBlocks()
 	{ // For registering Blocks
 		// Limestone
-		GameRegistry.registerBlock(ExampleMod.Limestone, "Limestone");// Needed
-																		// for
-																		// all
-																		// blocks
-		LanguageRegistry.addName(ExampleMod.Limestone, "Limestone Block");// In-game
-																			// name
+		GameRegistry.registerBlock(ExampleMod.Limestone, "Limestone");// Needed for all blocks
+		LanguageRegistry.addName(ExampleMod.Limestone, "Limestone Block");// In-game name
+		
+		GameRegistry.addRecipe(new ItemStack(ExampleMod.Limestone), "MMM", "ADA", "MDM", Character.valueOf('M'), Block.stone, Character.valueOf('A'), ExampleMod.Lime, Character.valueOf('D'), Item.stick);
+		GameRegistry.addSmelting(ExampleMod.Limestone.blockID, new ItemStack(ExampleMod.Lime), 0.1F);
+		
+		GameRegistry.registerBlock(ExampleMod.LandMine, "LandMine");
+		LanguageRegistry.addName(ExampleMod.LandMine, "LandMine");
+		
+		GameRegistry.addRecipe(new ItemStack(ExampleMod.Limestone), "PPP", "TTT", "SSS", Character.valueOf('P'), Block.pressurePlateStone, Character.valueOf('T'), Block.tnt, Character.valueOf('S'), Block.stoneSingleSlab);
 	}
 	
 	public void registerItems()
 	{ // For registering Items
-	
+		//Lime
+		LanguageRegistry.addName(ExampleMod.Lime, "Lime");
+		GameRegistry.addRecipe(new ItemStack(ExampleMod.Lime), "M M", " M ", "MMM", Character.valueOf('M'), Block.planks);
 	}
 }
