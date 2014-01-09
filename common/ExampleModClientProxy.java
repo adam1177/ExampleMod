17dcd964c90d6069cfdbce082608a8d2844c37db
Adam Blair <adamblair@rogers.com>
2014-01-08 22:10:52 -0500

    First commit
    Added general mod structure.

---
+++ common/ExampleModClientProxy.java
@@ -0,0 +1,11 @@
+package evar678.ExampleMod.common;
+
+import evar678.ExampleMod.common.ExampleModCommonProxy;
+import net.minecraftforge.client.MinecraftForgeClient;
+
+public class ExampleModClientProxy extends ExampleModCommonProxy {
+
+public void registerRenderInformation(){
+}
+
+}
